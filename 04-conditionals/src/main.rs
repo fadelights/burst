52a52535cfa037e84f007b18f7c75b9eb6d39822
Mani <fadelights@aut.ac.ir>
2024-05-04 16:37:43 +0330

    Update notes on conditionals

--- 04-conditionals/src/main.rs
+++ 04-conditionals/src/main.rs
@@ -1,5 +1,17 @@
+use std::thread::sleep;
+use std::time::Duration;
+
+
 fn main() {
-    let meaning = 42;
+    // branches(420);
+    // infinite_loop();
+    // break_loop(27);
+    label_loop()
+}
+
+
+fn branches(x: i32) {
+    let meaning = x;
 
     if meaning < 42 {
         println!("Heresy!");
@@ -8,4 +20,55 @@ fn main() {
     } else {
         println!("You may rest in peace.");
     }
+
+    // you can use expressions in `if` blocks, so the value is returned
+    let message = if meaning == 42 { "Passable." } else { "Heresy!" };
+    println!("{message}");
+}
+
+
+fn infinite_loop() {
+    // only stops when interrupted by ^C (Ctrl + C) or `break`
+    loop {
+        println!("Again!");
+        sleep(Duration::from_secs(1));
+    }
+}
+
+
+fn break_loop(x: isize) {
+    let mut counter = 0;
+    let result = loop {
+        if counter == x {
+            break counter;
+        }
+
+        counter += 1;
+    };
+
+    println!("Result: {result}");
+}
+
+
+fn label_loop() {
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count: {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining: {remaining}");
+            if remaining == 7 {
+                break;
+            }
+            if count == 5 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+
+    println!("Final count: {count}");
 }
--- 04-conditionals/src/notes.md
+++ 04-conditionals/src/notes.md
@@ -1,11 +1,15 @@
-A block is an expression that is allowed to contain statements.
+* A block is an expression that is allowed to contain statements.
 It also defines a syntactic scope for let-bindings inside it.
-
-```rust
-fn main() {
-    let x = {
-        let y = 2;
-        y + 1
-    };
-}
-```
+    ```rust
+    fn main() {
+        let x = {
+            let y = 2;
+            y + 1
+        };
+    }
+    ```
+* The loop keyword tells Rust to execute a block of code over and over again
+forever or until you explicitly tell it to stop.
+* The semicolon after `return` or `break` is optional.
+* `loop`s can be labeled. This will allow us to break an outer loop from
+within an inner loop.
