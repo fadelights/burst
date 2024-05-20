db5a5905b4ec5fd6a886a2b68f7569de2bb51f0b
Mani <fadelights@aut.ac.ir>
2024-05-20 12:11:09 +0330

    Add inline notes for guessing_game and update misc. notes

--- guessing_game/notes.md
+++ guessing_game/notes.md
@@ -1,2 +1,15 @@
+This project is meant to give a taste of the language.
+Concepts shown here are not explained but will
+be further taught in later chapters. Fret not if you don't
+quite understand what exactly is going on!
+
+# Misc. Notes
+- The _prelude_ is the list of things that Rust automatically imports
+into every Rust program.
+- You can find dependencies (crates) for your project from
+[crates.io](https://crates.io/). You can add them either by running
+`cargo add` or directly editing your `Cargo.toml` file. The next time
+`cargo build` is run, the dependencies will be automatically downloaded.
 - Running the `cargo doc --open` command will build documentation
 provided by all your dependencies locally and open it in your browser.
+- The `Cargo.lock` file ensures reproducibility of builds.
--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,9 +1,25 @@
+/* The `io` module is part of the standard library (`std`).
+You import it using the notation `std::io` and access it's
+functions using the same notation; e.g. `io::stdin()`.
+
+If we don't want to import a module, we can still use its
+functions by using the full specifier: `std::io::stdin()`
+
+To import multiple modules from the same library, you can use
+the {} notation as shown below. */
 use std::{cmp::Ordering, io};
+
+/* The `rand` library is a 3rd-party library.
+You can view it in the project's dependency list in Cargo.toml.
+
+While the `Rng` trait isn't explicitly used in the code, it
+defines methods that are implemented by random number generators
+and as such should be imported. */
 use rand::Rng;
 
-
 fn main() {
-    println!("Guess the number!");
+    println!("Guess the number!"); // Semicolons are a thing in Rust!
+
     let secret = rand::thread_rng().gen_range(1..=100);
 
     loop {
@@ -11,6 +27,7 @@ fn main() {
 
         let mut guess = String::new();
 
+        // You can have multi-line statements for increased readability
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line.");
@@ -20,7 +37,7 @@ fn main() {
                 println!("Not a number. Try again.");
                 continue;
             },
-            Ok(num) => { num },
+            Ok(num) => num, // Commas can be added at the end of lists too
         };
 
         match guess.cmp(&secret) {
