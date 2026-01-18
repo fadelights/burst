657d2b0042bab1957d49c749967426470a3b1c8b
Mani <mani@cartesianlabs.io>
2026-01-18 12:04:55 +0330

    Add another line of code for more examples of shadowing

--- 01-variables/src/main.rs
+++ 01-variables/src/main.rs
@@ -1,10 +1,13 @@
-const PI: f64 = 3.141592;
+const PI: f32 = 3.141592;
 
 fn main() {
     println!("The password for AUT's Math Faculty WiFi is: {PI}");
 
     let age = 13;
-    println!("A pretty young lad was {age} years old...");
+    println!("A pretty young lad was {age} years old.");
+
+    let age = age + 1;
+    println!("A year later, he became {age} years old...");
     {
         let age = 23;
         println!("But he grew to the age of {age} -- pretty old!");
