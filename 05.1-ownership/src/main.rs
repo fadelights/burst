8f8ffeb936d6b5b0572dd08f34acbf0de883c3a3
Mani <mani@cartesianlabs.io>
2026-02-09 11:26:58 +0330

    Update code sample for increased clarity

--- 05.1-ownership/src/main.rs
+++ 05.1-ownership/src/main.rs
@@ -1,12 +1,15 @@
 fn main() {
+    let a = [0; 10]; // this array lives on the stack
+    let mut b = a; // data is copied here
+    b[0] = 1;
+    println!("{:?}\n{:?}", a, b); // `a`'s data hasn't changed
+
+
     // allocate data on the heap using `Box`
     let a = Box::new([0; 1_000_000]);
     let b = a; // now they both point to the same location. `a` has been "moved"
     drop(b);
-
-    /* Using {:?} in a format string allows us to
-    print an array. */
-    // println!("{:?}", a); // can't do this because `a` has transferred ownership to `b`
+    // println!("{:?}", a); // ERROR: can't do this because `a` has transferred ownership to `b`
 
     // deep copy using `.clone()`
     let a = Box::new([1; 10]);
@@ -21,7 +24,8 @@ fn main() {
     // referencing and dereferencing
     let v = vec![1, 2, 3];
     let v0 = &v[0];
-    println!("{}, {}", v0, *v0); // implicit and explicit dereferencing
+    let &v1 = &v[1]; // dereferecing upon variable assignment
+    println!("{}, {}, {}", v0, *v0, v1); // implicit and explicit dereferencing
 }
 
 fn greet(name: &String) {
